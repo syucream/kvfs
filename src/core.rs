b45e366c6cbf7a85057332ff2b1cfa5c93a66064
Ryo Okubo <syucream1031@gmail.com>
2019-01-16 01:42:22 +0900

    Separate modules

---
+++ src/core.rs
@@ -0,0 +1,42 @@
+use std::ffi::OsStr;
+use libc::ENOENT;
+use fuse::{Filesystem, Request, ReplyAttr, ReplyData, ReplyDirectory, ReplyEntry, ReplyWrite};
+
+use driver::Driver;
+
+pub struct Kvfs {
+    // driver: Driver
+}
+
+/*
+impl Kvfs {
+    pub fn new(driver: Driver) -> Kvfs {
+        Kvfs {
+            driver: driver
+        }
+    }
+}
+*/
+
+// Super WIP
+impl Filesystem for Kvfs {
+    fn lookup(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEntry) {
+        reply.error(ENOENT);
+    }
+
+    fn getattr(&mut self, _req: &Request, _ino: u64, reply: ReplyAttr) {
+        reply.error(ENOENT);
+    }
+
+    fn read(&mut self, _req: &Request, _ino: u64, _fh: u64, _offset: i64, _size: u32, reply: ReplyData) {
+        reply.error(ENOENT);
+    }
+
+    fn readdir(&mut self, _req: &Request, _ino: u64, _fh: u64, _offset: i64, reply: ReplyDirectory) {
+        reply.error(ENOENT);
+    }
+
+    fn write(&mut self, _req: &Request, _ino: u64, _fh: u64, _offset: i64, _data: &[u8], _flags: u32, reply: ReplyWrite) {
+        reply.error(ENOENT);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ extern crate fuse;
 extern crate kvfs;
 
 use std::env;
-use kvfs::Kvfs;
+use std::path::Path;
+use kvfs::core::Kvfs;
+// use kvfs::driver::LevelDBDriver;
 
 fn main() {
     let mt = env::args_os().nth(1).unwrap();
 
-    let fs = Kvfs {};
+    // let path = Path::new("/tmp/kvfs");
+    // let driver = LevelDBDriver::new(path);
+
+    let fs = Kvfs {
+    };
 
     fuse::mount(fs, &mt, &[]).unwrap();
 }
