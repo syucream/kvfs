b453687c06dbc5592eb166c774270f742d9f7935
Ryo Okubo <syucream1031@gmail.com>
2019-01-17 00:00:24 +0900

    Use leveldb instance

--- src/drivers/leveldb.rs
+++ src/drivers/leveldb.rs
@@ -1,12 +1,14 @@
 extern crate leveldb as libleveldb;
 
 use std::path::Path;
-use drivers::driver::Driver;
-// use drivers::leveldb::libleveldb::database::Database;
+use drivers::leveldb::libleveldb::database::Database;
 use drivers::leveldb::libleveldb::options::Options;
+
+use drivers::driver::Driver;
 
 // LevelDB driver for Kvfs
 pub struct LevelDBDriver {
+    database: Database<i32>,
 }
 
 impl LevelDBDriver {
@@ -14,14 +16,14 @@ impl LevelDBDriver {
         let mut options = Options::new();
         options.create_if_missing = true;
 
-        /*
-        let mut db = match Database::open(path, options) {
+        let mut database = match Database::open(path, options) {
             Ok(db) => { db },
             Err(e) => { panic!("failed to open database: {:?}", e) }
         };
-        */
 
-        LevelDBDriver {}
+        LevelDBDriver {
+            database: database
+        }
     }
 }
 
